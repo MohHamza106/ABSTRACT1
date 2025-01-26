384fed37cddf32e03d5a3e05a96282d6a6d804cb
Mohamed hamza <MohHamza106>
2025-01-26 16:10:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿
+using ABSTRACT1.abstract1;
+
+namespace ABSTRACT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question1
+            #region 2
+            //D3point P= new D3point(10,10,10);
+            // Console.WriteLine(P);
+            #endregion
+            #region 3
+            //D3point P= new D3point(10,10,10);
+            // Console.WriteLine(P);
+            #endregion
+            #region 4
+            //Console.Write(" enter frist number:");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write(" enter second number:");
+            //bool result;
+            //int y;
+            //do
+            //{
+            //    result = int.TryParse(Console.ReadLine(), out y);
+            //}
+            //while (!result);
+            //Console.Write(" enter third number:");
+            //int z = Convert.ToInt32(Console.ReadLine());
+            //D3point P2 = new D3point(x, y, z);
+            //Console.WriteLine(P2);
+            #endregion
+            #region 5
+            //D3point P1 = new D3point(20, 80, 90);
+            //D3point P2 = new D3point(20, 80, 90);
+
+            //if (P1 == P2)
+            //{
+            //    Console.WriteLine("p1==p2");
+            //}
+            //else Console.WriteLine("p1!=p2");
+
+            #endregion
+            #region 6
+            //D3point[] arr = 
+            // {
+            //    new D3point(5,9,7),
+            //    new D3point(4,3,6),
+            //    new D3point(7,9,8),
+            //};
+            //Array.Sort(arr);
+            //foreach (D3point p in arr) 
+            //{
+            //    Console.WriteLine(p.ToString());
+            //}
+
+            #endregion
+            #region 7
+            //D3point P1 = new D3point(2,3,6);
+            //D3point P2 = new D3point(5,6,7);
+            //P2= (D3point)P1.Clone();
+            //Console.WriteLine(P2);
+
+            #endregion
+            #endregion
+
+
+
+
+
+        }
+    }
+}
---
+++ abstract1/3dpoint.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ABSTRACT1.abstract1
+{
+    internal class D3point :IComparable<D3point>,ICloneable
+    {
+        int point1 {  get; set; }
+        int point2 { get; set; }
+        int point3 {  get; set; }
+
+
+        public D3point()
+        {
+
+        }
+
+        public D3point( int x,int y,int z)
+        {
+            this.point1 = z;
+            this.point2= x;
+            this.point3 = y;
+        }
+        public static bool operator==(D3point a, D3point b)
+        {
+            if (a.point1==b.point1&&a.point2==b.point2&&a.point3==b.point3) return true;
+            return false;
+        }
+        public static bool operator !=(D3point a, D3point b)
+        {
+            if (a.point1 != b.point1 && a.point2 != b.point2 && a.point3 != b.point3) return true;
+            return false;
+        }
+        public override string ToString()
+        {
+            return  $"point1={point1},point2={point2},point3={point3}";
+        }
+
+        public int CompareTo(D3point? other)
+        {
+            return this.point1.CompareTo(other.point1);
+            return this.point2.CompareTo(other.point2);
+        }
+
+        public object Clone()
+        {
+            return new D3point()
+            {
+                point1 = this.point1,
+               point2= this.point2,
+               point3 = this.point3
+            };
+        }
+    }
+}
